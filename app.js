0ef013499d77e8840bfc9554ae2d6e6649ed20cb
Myke Bueno <myke159951@hotmail.com>
2025-02-05 16:34:55 -0300

    add adiconar nomes

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// nomes
+let amigos = []
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+    if (nomeAmigo == "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(nomeAmigo);
+        document.getElementById('amigo').value = '';
+        console.log(amigos);
+
+    }
+    gerarListaAmigos()
+}
+
+
+function gerarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(function (amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li)
+    })
+
+}
